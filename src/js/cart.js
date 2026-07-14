890e8f86cf85d18da361418dba213589d6ddb0ec
iketlenglesedi6-cmd <iketlenglesedi6@gmail.com>
2026-07-14 18:53:28 +0200

    Add remove item from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,18 +5,33 @@ function clearCart() {
   renderCartContents();
 }
 
+function removeItem(index) {
+  const cartItems = getLocalStorage("so-cart") || [];
+  const items = Array.isArray(cartItems) ? cartItems : [cartItems];
+  items.splice(index, 1);
+  setLocalStorage("so-cart", items);
+  renderCartContents();
+}
+
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
   const items = Array.isArray(cartItems) ? cartItems : [cartItems];
-  const htmlItems = items.map((item) => cartItemTemplate(item));
+  const htmlItems = items.map((item, index) => cartItemTemplate(item, index));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  document.querySelectorAll(".cart-card__remove").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const index = e.target.dataset.index;
+      removeItem(index);
+    });
+  });
 }
 
-function cartItemTemplate(item) {
+function cartItemTemplate(item, index) {
   const newItem = `<li class='cart-card divider'>
   <a href='#' class='cart-card__image'>
     <img
-src='${item.Image}'
+      src='${item.Image}'
       alt='${item.Name}'
     />
   </a>
@@ -26,6 +41,7 @@ src='${item.Image}'
   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
   <p class='cart-card__quantity'>qty: 1</p>
   <p class='cart-card__price'>$${item.FinalPrice}</p>
+  <button class='cart-card__remove' data-index='${index}'>Remove</button>
 </li>`;
 
   return newItem;
