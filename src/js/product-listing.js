aa8de495f9fd6229886060e6b24ed8a78711ac46
devdelali <delalpaul5@gmail.com>
2026-07-21 09:52:19 +0000

    Expand Inventory WK03

---
+++ src/js/product-listing.js
@@ -0,0 +1,27 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import Alert from "./Alert.js";
+import { getWeatherRecommendation, highlightWeatherMatch } from "./weatherRecommend.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+const dataSource = new ProductData("tents");
+const listElement = document.querySelector(".product-list");
+
+const myList = new ProductList("tents", dataSource, listElement);
+myList.init();
+
+const alert = new Alert();
+alert.showAlerts();
+
+async function initWeather() {
+    try {
+        const weather = await getWeatherRecommendation();
+        highlightWeatherMatch(weather);
+    } catch {
+        // location not available or denied, skip the feature
+    }
+}
+
+initWeather();
+loadHeaderFooter();
+
